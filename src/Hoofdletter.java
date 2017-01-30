8946180f41ba82746a30785a6a8cc8d6bf9e0849
IrisSaskia <ioostra@roc-dev.com>
2017-01-30 13:53:02 +0100

    Initial commit

---
+++ src/Hoofdletter.java
@@ -0,0 +1,8 @@
+/**
+ * Created by iris5 on 30-1-2017.
+ */
+public class Hoofdletter {
+        public static void main(String[] args) {
+            System.out.println("Dit is mijn eerste java klasse!");
+        }
+}
